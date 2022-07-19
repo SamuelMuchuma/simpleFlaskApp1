c21872d2d311b6d29f8787b0500061491ec38b2b
unknown <samuelmuchuma@gmail.com>
2022-07-19 12:33:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, flash
+import os
+
+app = Flask(__name__)
+# initializing the secrete key
+app.secret_key = os.urandom(24)
+
+
+# call the index.html template when the url is http://localhost:5000/
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# flash message after button click in the new template display.html
+@app.route("/variable1", methods=["POST", "GET"])
+def btnsubmit():
+    flash(" Hello " + str(request.form['name_input']) + " !! You are " + str(request.form['age_input']) + " years old!")
+    return render_template("display.html")
+
+
+# call the index.html template when the url is http://localhost:5000/display.html
+@app.route("/display.html")
+def btnback():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
